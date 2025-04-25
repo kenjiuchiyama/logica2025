56e62175ace7aab96dc46f9f4f96570c689af23f
Henrique Kenji Uchiyama <hkenjiu@gmail.com>
2025-04-25 15:06:25 -0300

    Create 1153.c

---
+++ 1153.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, A;
+    scanf("%d", &N);
+    A = N;
+    while(A>1){
+        N = N * (A - 1);
+        A = A - 1;
+    }
+    printf("%d\n", N);
+    
+    return 0;
+}
