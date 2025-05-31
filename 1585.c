726d51e36b6f84e0847ddfbc0a1ced31cf6e6072
Henrique Kenji Uchiyama <hkenjiu@gmail.com>
2025-05-31 16:27:24 -0300

    Create 1585.c

---
+++ 1585.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N, x, y;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &x, &y);
+        printf("%d cm2\n", x * y / 2);
+    }
+
+    return 0;
+}
